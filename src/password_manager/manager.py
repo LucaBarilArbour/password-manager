9e46ffac5cd6d31b3ada092280902c52c57dc4ac
LucaBarilArbour <2563230@cegepoutaouais.qc.ca>
2025-10-09 14:12:41 -0400

    feat: terminer le prompt

--- src/password_manager/manager.py
+++ src/password_manager/manager.py
@@ -1,24 +1,38 @@
-import os, json, sys, hashlib 
-# from prompt_toolkit import prompt
-# from prompt_toolkit.document import Document
-# from prompt_toolkit.validation import Validator, ValidationError
+import os, json, sys, hashlib, json
+from prompt_toolkit import prompt
+from prompt_toolkit.document import Document
+from prompt_toolkit.validation import Validator, ValidationError
+
+def modifier_mdp(mdp):
+    """
+    :return: mot de passe modifier 
+    """
+    mdp = prompt("modifier le mot de passe: ", default=mdp)
+    return mdp
 
 def creation_master_password():
-    mot_de_passe = input("Entré un mot de passe: ")
-    mot_de_passe_a_confirmer = input("Confirmer le mot de passe: ")
-    # if len(mot_de_passe) <= 12:
-        # confirmation_mot_de_passe_faible = input("Votre mot de passe est faible.\n Voulez vous continuer avec un mot de passe faible?(y/n): ")
-        # if confirmation_mot_de_passe_faible == "y":
+    mot_de_passe = prompt("Entré un mot de passe: ", is_password=True)
+    if mot_de_passe == "":
+        print("Le mot de passe est vide il faut mettre un mot_de_passe")
+        mot_de_passe = prompt("Entré un mot de passe: ", is_password=True)
 
+    if len(mot_de_passe) <= 12:
+        choix_mdp_faible = input("Votre mot de passe est faible.\n Voulez vous continuer avec un mot de passe faible?(y/n): ")
+        if choix_mdp_faible == "n":
+            mot_de_passe = modifier_mdp(mot_de_passe)
+    mot_de_passe_a_confirmer = prompt("Confirmer le mot de passe: ", is_password=True)
 
     if mot_de_passe == mot_de_passe_a_confirmer:
         mot_de_passe_confirmer = mot_de_passe
     else:
         valide = False
-        while valide == False:
+        while valide is False:
             print("mot de passe")
             mot_de_passe_a_confirmer = input("Confirmer le mot de passe: ")
             if mot_de_passe == mot_de_passe_a_confirmer:
                 valide = True
-    return mot_de_passe
-creation_master_password()
+        mot_de_passe_confirmer = mot_de_passe
+
+    return hashlib.sha256(mot_de_passe_confirmer.encode()).hexdigest()
+
+print(creation_master_password())
